b49e48052a1e6595dead2d4aff05a0525510ef36
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-06-15 14:35:17 +0500

    Day 40

---
+++ main.ts
@@ -0,0 +1,31 @@
+//         🚀 Day 40 Challenge: Start Coding! 🚀
+
+// Question 118: 
+// Write a loop that logs numbers from 1 to 10 to the console.
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// Question 119: 
+// Create a while loop that logs "Hello, World!" 5 times.
+let count: number = 0;
+
+while (count < 5) {
+    count++; // increament count by 1
+    console.log("Hello, World!");
+}
+
+
+// Question 120: 
+// Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+
+let movies: string[] = ["The Godfather", "The Shawshank Redemption", "The Godfather Part II"];
+
+for (let movie of movies){
+    console.log(movie);
+}
+
